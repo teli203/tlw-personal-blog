d44bf588b4e0f207c6b657d1e3e7a74b4876d439
teli203 <teli203@aol.com>
2025-02-18 10:51:32 -0600

    Add JS code for both home buttons, but due to this project being used as an example for future coders I won't reconfigure the whole layout with js function in place of all the a href links

---
+++ assets/js/blog.js
@@ -0,0 +1,7 @@
+document.getElementById("homeBtn").addEventListener("click", function() {
+    window.location.href = "/index.html"; // Navigate to index.html
+});
+
+document.getElementById("homeBtn2").addEventListener("click", function() {
+    window.location.href = "/index.html"; // Navigate to index.html
+});
